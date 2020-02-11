8d23663f1d7cb5e5fd3d9badacd3dbc0ebdf8f91
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-02-11 12:54:07 +0000

    updated the when.js to work with the dates.

--- src/components/shared/Tray/TrayComponents/When/When.js
+++ src/components/shared/Tray/TrayComponents/When/When.js
@@ -2,7 +2,8 @@ import React, { useContext } from 'react';
 import DatePicker from 'react-datepicker'; // Uses https://reactdatepicker.com/
 
 // Import contexts
-import { WhenContext } from 'globalState';
+import { WhenContext, FetchDisruptionsContext } from 'globalState';
+//import { WhenContext } from 'globalState';
 // Import components
 import Button from 'components/shared/Button/Button';
 // Import styles
@@ -12,9 +13,37 @@ import './datePicker.scss';
 
 const When = () => {
   const [whenState, whenDispatch] = useContext(WhenContext); // Get the state of whenButtons from WhenContext
+  const [fetchDisruptionsState] = useContext(FetchDisruptionsContext);
+  // Get the state of disruptionsApi from fetchDisruptionsState
 
   const today = new Date(); // Get today's date
   const nowText = `Now ${today.getHours()}:${today.getMinutes()}`; // Set nowText to be 'Now HH:MM'
+
+  //Mapping over the start and end times
+  //Empty Arr
+  const NewDateRange = [];
+  //.map over all the dates that have disruptions
+  fetchDisruptionsState.data.map(function(test) {
+    //Push on to dateRange arr
+    NewDateRange.push(test.disruptionTimeWindow);
+  });
+  //Whats in our new arr?
+  console.log('What OUT side of the map funx is in our new arr? - ', NewDateRange);
+
+  //Trying to get the dates to be edited - all seven days?
+  const highlightWithRanges = [
+    {
+      'react-datepicker__day--highlighted-custom-1': [
+        NewDateRange(new Date(), 7),
+        NewDateRange(new Date(), 6),
+        NewDateRange(new Date(), 5),
+        NewDateRange(new Date(), 4),
+        NewDateRange(new Date(), 3),
+        NewDateRange(new Date(), 2),
+        NewDateRange(new Date(), 1)
+      ]
+    }
+  ];
 
   return (
     <div className="wmnds-grid">
@@ -55,6 +84,7 @@ const When = () => {
               onChange={date => whenDispatch({ type: 'UPDATE_CUSTOMDATE', date })}
               inline
               calendarClassName="disruptions-date-picker"
+              highlightDates={highlightWithRanges}
             />
           </div>
         </div>
